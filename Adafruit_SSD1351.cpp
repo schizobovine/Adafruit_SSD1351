8f8e699a28b4e3782317a045dc24120617518723
Sean Caulfield <sean@yak.net>
2016-05-27 12:35:51 -0400

    using cast hammer to get compiling

--- Adafruit_SSD1351.cpp
+++ Adafruit_SSD1351.cpp
@@ -462,16 +462,16 @@ Adafruit_SSD1351::Adafruit_SSD1351(uint8_t cs, uint8_t rs, uint8_t sid, uint8_t 
     _sclk = sclk;
     _rst = rst;
     
-    csport      = portOutputRegister(digitalPinToPort(cs));
+    csport      = (uint8_t*)portOutputRegister(digitalPinToPort(cs));
     cspinmask   = digitalPinToBitMask(cs);
 
-    rsport      = portOutputRegister(digitalPinToPort(rs));
+    rsport      = (uint8_t*)portOutputRegister(digitalPinToPort(rs));
     rspinmask   = digitalPinToBitMask(rs);
 
-    sidport      = portOutputRegister(digitalPinToPort(sid));
+    sidport      = (uint8_t*)portOutputRegister(digitalPinToPort(sid));
     sidpinmask   = digitalPinToBitMask(sid);
 
-    sclkport      = portOutputRegister(digitalPinToPort(sclk));
+    sclkport      = (uint8_t*)portOutputRegister(digitalPinToPort(sclk));
     sclkpinmask   = digitalPinToBitMask(sclk);
 
 }
@@ -483,10 +483,10 @@ Adafruit_SSD1351::Adafruit_SSD1351(uint8_t cs, uint8_t rs,  uint8_t rst) : Adafr
     _sclk = 0;
     _rst = rst;
 
-    csport      = portOutputRegister(digitalPinToPort(cs));
+    csport      = (uint8_t*)portOutputRegister(digitalPinToPort(cs));
     cspinmask   = digitalPinToBitMask(cs);
     
-    rsport      = portOutputRegister(digitalPinToPort(rs));
+    rsport      = (uint8_t*)portOutputRegister(digitalPinToPort(rs));
     rspinmask   = digitalPinToBitMask(rs);
 
 }
